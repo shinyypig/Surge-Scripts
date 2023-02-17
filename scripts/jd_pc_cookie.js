23434b27153c6469669aaffdcac245553422df75
Liangliang Zhu <shinyypig@gmail.com>
2023-02-17 19:46:21 +0800

    📝 添加jd_pc_cookie说明

--- scripts/jd_pc_cookie.js
+++ scripts/jd_pc_cookie.js
@@ -1,5 +1,5 @@
 console.log("检测到京东网页");
 let cookie = JSON.stringify($request.headers.cookie);
 console.log("获取到cookie:\n " + cookie);
-$notification.post('成功获取到Cookie', '到Dashboard或log中复制完整Cookie', cookie);
+$notification.post('成功获取到Cookie', '到log中复制完整Cookie', cookie);
 $done({});
