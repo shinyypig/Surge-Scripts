cfed29861981db8df73999fc96d054606b75b826
Liangliang Zhu <shinyypig@gmail.com>
2023-02-17 19:30:35 +0800

    🩹 完善脚本

--- scripts/jd_pc_cookie.js
+++ scripts/jd_pc_cookie.js
@@ -1,4 +1,5 @@
-cookie = JSON.stringify($request.headers.cookie);
+console.log("检测到京东网页");
+let cookie = JSON.stringify($request.headers.cookie);
 console.log("获取到cookie:\n " + cookie);
-$notification.post('检测到京东网页', '成功获取到Cookie', cookie);
+$notification.post('成功获取到Cookie', '到Dashboard或log中复制完整Cookie', cookie);
 $done({});
